0df1a23200f2ead56e0ba6258ddcea4a88590031
alphaCoder-69 <eldrago2000@gmail.com>
2020-08-09 16:25:27 +0530

    Version 2

--- README.md
+++ README.md
@@ -1,10 +1,5 @@
-# ToDo-List
+Download the required npm packages before running this project.
+This package needs MongoDB installed in the system to work.
 
-<p>This is a simple light-weight ToDo-List app made using Express & Ejs. This Nodejs app is fully functional and is very minimalistic.
-</p>
-
-![alt text](https://github.com/alphaCoder-69/ToDo-List/blob/master/SS-1.jpg?raw=true)
-
-![alt text](https://github.com/alphaCoder-69/ToDo-List/blob/master/SS-2.jpg?raw=true)
-
-![alt text](https://github.com/alphaCoder-69/ToDo-List/blob/master/SS-3.jpg?raw=true)
+link:
+localhost:5000/
--- app.js
+++ app.js
@@ -2,8 +2,31 @@
 
 const express=require("express");
 const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
+const date=require(__dirname+"/date.js");
 
 const app=express();
+
+let pointData=["Wake Up!","Learn","Repeat"];
+let workPoint=["Do Homework"];
+let count=4;
+let k=1;
+
+// Setting up our database
+mongoose.connect("mongodb://localhost:27017/toDo_list",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const dataSchema=new mongoose.Schema({
+    _id:Number,
+    item:{
+        type:String,
+        required:[1,"The field should not be blank"]
+    }
+});
+const dailyData=mongoose.model("Data",dataSchema); // Data collection created
+
+
 
 // Setting up ejs
 app.set("view engine","ejs");
@@ -14,28 +37,80 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"));
 
-var pointData=["Wake Up!","Learn","Repeat"];
 
-app.post("/add",function(req,res) {
-    var Data=req.body.point;
-    pointData.push(Data);
+//  Normal List : localhost:5000
+app.post("/list",function(req,res) {
+    const newItem= new dailyData({
+        _id:count,
+        item:req.body.point
+    });
+    newItem.save();
+    count++;
+    dailyData.find((err,data)=>{
+        if(err){
+            console.log(err);
+        }else{
+            pointData=data;
+        }
+    });
+
     res.redirect("/");
 });
 
-app.get("/",function(req,res) {
-    var options={
-        month:"long",
-        day:"2-digit",
-        weekday:"long"
-    };
-    var today=new Date();
-    var day=today.toLocaleDateString("en-us",options);
 
-    res.render("ToDo",{ //an object which tells us the the values to be substituted
-        kindOfDay:day,
-        newPoint:pointData
+app.get("/",function(req,res) {
+    let day=date.getdate();
+    // Updating the default data in the database if there no data
+    dailyData.find((err,data)=>{
+        if(err){
+            console.log(err);
+        }
+        if(data.length==0){
+            for(var i=0;i<3;i++){
+                const dataDefault=new dailyData({
+                    _id:i+1,
+                    item:pointData[i]
+                });
+                dataDefault.save();
+                if(i==2){console.log("Data saved successfully");}
+            }
+            res.render("ToDo",{ //an object which tells us the the values to be substituted
+                title:day,
+                newPoint:pointData,
+                dataPush:"/list"
+            });
+        }
+        if(data.length!=0){
+            count=data.length + 1;
+            for(let i=0;i<data.length;i++){
+                pointData[i]=data[i].item;
+            }
+            res.render("ToDo",{ //an object which tells us the the values to be substituted
+                title:day,
+                newPoint:pointData,
+                dataPush:"/list"
+            });
+        }
     });
 });
+
+
+ //  Work List : localhost:5000/work
+app.get("/work",function(req,res) {
+
+    res.render("ToDo",{
+        title:"Work List",
+        newPoint:workPoint,
+        dataPush:"/workList"
+    });
+});
+
+app.post("/workList",function(req,res) {
+    var Data=req.body.point;
+    workPoint.push(Data);
+    res.redirect("/work");
+});
+
 
 app.listen(5000,function() {
     console.log("Server is online at port 5000");
