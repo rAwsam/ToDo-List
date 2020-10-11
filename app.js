d6b1f0ea01eb987810c8dbd4b7367f23777ddfc9
alphaCoder-69 <eldrago2000@gmail.com>
2020-10-11 20:21:26 +0530

    Refractor

--- app.js
+++ app.js
@@ -16,210 +16,227 @@ let day = date.getdate();
 // Setting up ejs
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(express.static("public"));
 
 // Setting up our database
 mongoose.connect("mongodb://localhost:27017/toDo_list", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
-mongoose.set("useFindAndModify",false); // Wrote this becoz of some deprection warnings.
+mongoose.set("useFindAndModify", false); // Wrote this becoz of some deprection warnings.
 
 const dataSchema = new mongoose.Schema({
-    _id: Number,
-    item: {
-        type: String,
-        required: [1, "The input field should not be blank"]
-    }
+  _id: Number,
+  item: {
+    type: String,
+    required: [1, "The input field should not be blank"],
+  },
 });
 const dailyData = mongoose.model("List", dataSchema);
 // Data collection created for default page
 
-
 const userSchema = new mongoose.Schema({
-    name: String,
-    points: [dataSchema]
+  name: String,
+  points: [dataSchema],
 });
 const userData = new mongoose.model("Point", userSchema);
 // Data collection for different users
 
-
 // Creating objects for lists collection
 const item1 = new dailyData({
-    _id: 1,
-    item: "To Add click: +"
+  _id: 1,
+  item: "To Add click: +",
 });
 const item2 = new dailyData({
-    _id: 2,
-    item: "To Delete click the checkbox"
+  _id: 2,
+  item: "To Delete click the checkbox",
 });
 const item3 = new dailyData({
-    _id: 3,
-    item: "Type /urName in url for ur own list"
+  _id: 3,
+  item: "Type /urName in url for ur own list",
 });
-
 
 // Here both the requests are handled for the home route and the users route
-app.post("/list", function(req, res) {
-    var listName = req.body.add;
+app.post("/list", function (req, res) {
+  var listName = req.body.add;
 
-    if (listName != day) { // enters into users endpoints
-        const newItem = {
-            _id: userId,
-            item: req.body.point
-        };
+  if (listName != day) {
+    // enters into users endpoints
+    const newItem = {
+      _id: userId,
+      item: req.body.point,
+    };
 
-        userData.findOne({
-            name: listName
-        }, (err, foundList) => {
-            var pointArray = foundList.points;
-            pointArray.push(newItem);
+    userData.findOne(
+      {
+        name: listName,
+      },
+      (err, foundList) => {
+        var pointArray = foundList.points;
+        pointArray.push(newItem);
 
-            userData.updateOne({
-                name: listName
-            }, {
-                points: pointArray
-            }, (err) => {
-                if (!err) {
-                    console.log(listName + ": Item added");
-                    userId++;
-                }
-            });
-            res.render("ToDo.ejs", {
-                title: listName,
-                newPoint: foundList.points
-            });
-
-        });
-
-    } else { // enters into default endpoints
-        const newItem = new dailyData({
-            _id: count,
-            item: req.body.point
-        });
-        newItem.save();
-        count++;
-        dailyData.find((err, data) => {
+        userData.updateOne(
+          {
+            name: listName,
+          },
+          {
+            points: pointArray,
+          },
+          (err) => {
             if (!err) {
-                console.log("New Point added");
+              console.log(listName + ": Item added");
+              userId++;
             }
-            res.redirect("/");
+          }
+        );
+        res.render("ToDo.ejs", {
+          title: listName,
+          newPoint: foundList.points,
         });
-    }
+      }
+    );
+  } else {
+    // enters into default endpoints
+    const newItem = new dailyData({
+      _id: count,
+      item: req.body.point,
+    });
+    newItem.save();
+    count++;
+    dailyData.find((err, data) => {
+      if (!err) {
+        console.log("New Point added");
+      }
+      res.redirect("/");
+    });
+  }
 });
-
 
 // Deleting an item
 app.post("/del", (req, res) => {
-    var itemNo = req.body.checkBox;
-    var userName = req.body.listName;
+  var itemNo = req.body.checkBox;
+  var userName = req.body.listName;
 
-    if (userName == day) { // enters into default endpoints for deletion
-        dailyData.deleteOne({
-            _id: itemNo
-        }, (err) => {
-            if (!err) {
-                console.log("Item deleted");
-            }
-        });
-        dailyData.find((err, data) => {
-            if (data.length == 0) {
-                adder++;
-                res.redirect("/");
-            } else {
-                adder = 0;
-                res.redirect("/");
-            }
-        });
-    } else { // enters into users endpoints for deletion
-        userData.findOneAndUpdate({
-            name: userName
-        }, {
-            $pull: {
-                points: {
-                    _id: itemNo
-                }
-            }
-        }, (err, foundList) => {
-            if (!err) {
-                console.log(userName + ": Database Updated!");
-                res.redirect("/" + userName);
-            }
-        });
-    }
-
+  if (userName == day) {
+    // enters into default endpoints for deletion
+    dailyData.deleteOne(
+      {
+        _id: itemNo,
+      },
+      (err) => {
+        if (!err) {
+          console.log("Item deleted");
+        }
+      }
+    );
+    dailyData.find((err, data) => {
+      if (data.length == 0) {
+        adder++;
+        res.redirect("/");
+      } else {
+        adder = 0;
+        res.redirect("/");
+      }
+    });
+  } else {
+    // enters into users endpoints for deletion
+    userData.findOneAndUpdate(
+      {
+        name: userName,
+      },
+      {
+        $pull: {
+          points: {
+            _id: itemNo,
+          },
+        },
+      },
+      (err, foundList) => {
+        if (!err) {
+          console.log(userName + ": Database Updated!");
+          res.redirect("/" + userName);
+        }
+      }
+    );
+  }
 });
-
 
 //  Normal List : localhost:5000
 app.get("/", (req, res) => {
-    // Updating the default data in the database if there no data
-    dailyData.find((err, data) => {
-        if (data.length == 0 && adder == 0) {
-            dailyData.insertMany([item1, item2, item3], (err) => {
-                if (err) {
-                    return;
-                } else {
-                    console.log("Save successful");
-                }
-            });
-            res.redirect("/");
+  // Updating the default data in the database if there no data
+  dailyData.find((err, data) => {
+    if (data.length == 0 && adder == 0) {
+      dailyData.insertMany([item1, item2, item3], (err) => {
+        if (err) {
+          return;
         } else {
-            res.render("ToDo", {
-                title: day,
-                newPoint: data
-            });
+          console.log("Save successful");
         }
-    });
+      });
+      res.redirect("/");
+    } else {
+      res.render("ToDo", {
+        title: day,
+        newPoint: data,
+      });
+    }
+  });
 });
-
 
 // HAndles get requests from the users ToDo lists
 app.get("/:userName", (req, res) => {
-    let userName = req.params.userName;
-    let UserName = _.capitalize(userName);
-    const defaultObj = new userData({
-        name: UserName,
-        points: [{
-            _id: userId,
-            item: "ToDo List"
-        }]
-    });
+  let userName = req.params.userName;
+  let UserName = _.capitalize(userName);
+  const defaultObj = new userData({
+    name: UserName,
+    points: [
+      {
+        _id: userId,
+        item: "ToDo List",
+      },
+    ],
+  });
 
-    userData.findOne({
-        name: UserName
-    }, (err, foundList) => {
-        if (!err && !foundList && userName != "favicon.ico") {
-            defaultObj.save();
-            console.log(UserName + ": Default point created");
-            userId++;
-            res.render("ToDo.ejs", {
+  userData.findOne(
+    {
+      name: UserName,
+    },
+    (err, foundList) => {
+      if (!err && !foundList && userName != "favicon.ico") {
+        defaultObj.save();
+        console.log(UserName + ": Default point created");
+        userId++;
+        res.render("ToDo.ejs", {
+          title: UserName,
+          newPoint: defaultObj.points,
+        });
+      } else {
+        if (userName != "favicon.ico") {
+          userId++;
+          console.log(UserName + ": Default Point exists");
+          userData.findOne(
+            {
+              name: UserName,
+            },
+            (err, foundList) => {
+              res.render("ToDo.ejs", {
                 title: UserName,
-                newPoint: defaultObj.points
-            });
-        } else {
-            if (userName != "favicon.ico") {
-                userId++;
-                console.log(UserName + ": Default Point exists");
-                userData.findOne({
-                    name: UserName
-                }, (err, foundList) => {
-                    res.render("ToDo.ejs", {
-                        title: UserName,
-                        newPoint: foundList.points
-                    });
-                });
+                newPoint: foundList.points,
+              });
             }
+          );
         }
-
-    });
+      }
+    }
+  );
 });
 
-
 app.listen(5000, () => {
-    console.log("Server is online at port 5000");
+  console.log("Server is online at port 5000");
 });
